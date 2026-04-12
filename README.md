8669825b609c00ed7321836d26a1a817627f6c78
spamegg <spamegg@pm.me>
2026-04-12 14:00:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# N2t4s
+
+[Nand2Tetris](https://www.nand2tetris.org/) in Scala,
+with [Chisel](https://www.chisel-lang.org/)
+
+## Work in progress
+
+Stay tuned!
---
+++ project.scala
@@ -0,0 +1,4 @@
+//> using scala 3.8.3
+//> using toolkit 0.9.2
+//> using dep org.chipsalliance:chisel_2.13:7.10.0
+//> using options -explain-cyclic -opt -opt-inline:**
