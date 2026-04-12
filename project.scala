c7dabba4e95ea9e9ce964aae188f0e280524fb7b
spamegg <spamegg@pm.me>
2026-04-12 14:03:53 +0300

    add workflows

--- project.scala
+++ project.scala
@@ -2,3 +2,16 @@
 //> using toolkit 0.9.2
 //> using dep org.chipsalliance:chisel_2.13:7.10.0
 //> using options -explain-cyclic -opt -opt-inline:**
+
+//> using publish.organization io.github.spamegg1
+//> using publish.name n2t4s
+//> using publish.license Apache-2.0
+//> using publish.computeVersion git:tag
+//> using publish.developers "spamegg1|Spamegg|https://github.com/spamegg1"
+//> using publish.url https://github.com/spamegg1/n2t4s
+//> using publish.vcs github:spamegg1/n2t4s
+//> using publish.repository central
+//> using publish.user env:SONATYPE_USERNAME
+//> using publish.password env:SONATYPE_PASSWORD
+//> using publish.secretKey env:PGP_SECRET
+//> using publish.secretKeyPassword env:PGP_PASSPHRASE
